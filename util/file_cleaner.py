553f1f25d5e3730ac34a5643db00df0dc18604a2
pmwindcl <57667144+rihalvnium76@users.noreply.github.com>
2026-07-14 04:06:38 +0800

    Update file_cleaner.py

--- util/file_cleaner.py
+++ util/file_cleaner.py
@@ -1,5 +1,10 @@
+import argparse
+from dataclasses import dataclass
 from datetime import datetime
+from genericpath import isfile
 import logging
+import os
+from os import path
 from pathlib import Path
 import sys
 import time
@@ -22,107 +27,232 @@ class LogFormatter(logging.Formatter):
         record.levelname = record.levelname[0]
         return super().format(record)
 
+class LogFormatter(logging.Formatter):
+    HEADER_COLORS = {
+        "D": "\033[34m",
+        "I": "\033[32m",
+        "W": "\033[93m",
+        "E": "\033[91m",
+        "C": "\033[91m",
+    }
+    DEFAULT_HEADER_COLOR = "\033[97m"
+    RESET_STYLE = "\033[0m"
+
+    color_output = True
+
+    def format(self, record):
+        record.levelname = level = record.levelname[0]
+
+        if self.color_output:
+            record.header_color = self.HEADER_COLORS.get(level, self.DEFAULT_HEADER_COLOR)
+            record.reset_style = self.RESET_STYLE
+        else:
+            record.header_color = ""
+            record.reset_style = ""
+
+        return super().format(record)
+
 def build_logger():
     handler = logging.StreamHandler()
     formatter = LogFormatter(
-        fmt='[%(levelname)s %(asctime)s] %(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S',
+        fmt='%(header_color)s[%(levelname)s %(asctime)s]%(reset_style)s %(message)s',
+        # full: %Y-%m-%d %H:%M:%S
+        datefmt='%H:%M:%S',
     )
     handler.setFormatter(formatter)
     logger = logging.getLogger(__name__)
     logger.addHandler(handler)
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(logging.INFO)
     return logger
 
 log = build_logger()
 
-class FileCleaner:
-    def clean(
-        self,
-        cleanable: bool,
-        target_dir: Path,
-        # second
-        start_timestamp: int | None = None,
-        # second
-        end_timestamp: int | None = None,
-    ):
-        count = 0
-        size = 0
+class DateTimeUtils:
+    DATETIME_PATTERNS = [
+        "%Y-%m-%d %H:%M:%S",
+        "%Y-%m-%d",
+        "%Y%m%d %H%M%S",
+        "%Y%m%d",
+    ]
 
-        target_files: list[Path] = []
-        max_recorded_file_count = 10
+    @classmethod
+    def to_mtime(cls, v: str):
+        for pattern in cls.DATETIME_PATTERNS:
+            try:
+                return datetime.strptime(v, pattern).timestamp()
+            except ValueError:
+                pass
 
-        last_log = time.time()
-        interval = 5
-        
-        action = "已发现"
-        if cleanable:
-            action = "已删除"
-        
-        if start_timestamp is None and end_timestamp is None:
-            log.error("日期范围不能均为空")
-            sys.exit(1)
+class Params(argparse.Namespace):
+    dir: str
+    after: float | None
+    before: float | None
+    yes: bool
+    color: bool
 
-        for root, dirs, files in target_dir.walk():
-            for file in files:
-                file = root / file
-                stat = file.stat()
-                mtime = stat.st_mtime
+@dataclass
+class File:
+    path: str
+    size: int
+    mtime: float
 
-                if (start_timestamp is None or mtime >= start_timestamp) and (end_timestamp is None or mtime < end_timestamp):
-                    if cleanable:
-                        try:
-                            file.unlink()
-                        except FileNotFoundError:
-                            continue
-
-                    count += 1
-                    size += stat.st_size
-
-                    if not cleanable and len(target_files) < max_recorded_file_count:
-                        target_files.append(file)
-                    
-                    if time.time() - last_log >= interval:
-                        log.info(f"{action} {count} 个文件，共计 {FileSizeUtils.format(size)}")
-                        last_log = time.time()
-        
-        log.info(f"{action} {count} 个文件，共计 {FileSizeUtils.format(size)}")
-        for file in target_files:
-            log.debug(f"- {file.as_posix()}")
-        
-        log.info("完成")
-    
-    def run(self):
-        date_pattern = "%Y%m%d"
-
-        log.debug(f"argv: {sys.argv}")
-
-        args = iter(sys.argv)
-        cleanable = False
-        target_dir = Path(r"D:\Documents\Tencent Files\123456879\Image")
-        start_timestamp = None
-        end_timestamp = None
-
-        for arg in args:
-            if arg == "-D":
-                cleanable = True
-            elif arg == "-p":
-                target_dir = Path(next(args))
-            elif arg == "-a":
-                start_timestamp = int(datetime.strptime(next(args), date_pattern).timestamp())
-            elif arg == "-b":
-                end_timestamp = int(datetime.strptime(next(args), date_pattern).timestamp())
-        
-        if target_dir is None:
-            log.error("目标目录不能为空")
-            sys.exit(1)
-
-        self.clean(
-            cleanable=cleanable,
-            target_dir=target_dir,
-            start_timestamp=start_timestamp,
-            end_timestamp=end_timestamp,
+    @classmethod
+    def from_stat(cls, path: str, stat: os.stat_result):
+        return cls(
+            path=path,
+            size=stat.st_size,
+            mtime=stat.st_mtime
         )
 
+
+class Progress:
+    interval = 2
+
+    def __init__(
+        self,
+        total_count = 0,
+        name = "",
+    ):
+        self.total_count = f"/{total_count}" if total_count else ""
+        self.name = f"- ({name}) " if name else "- "
+
+        self.unmodified = True
+        self.acc_count = 0
+
+        self.last_time = time.monotonic()
+        self.last_count = 0
+        self.last_size = 0
+
+    def output(self, check_interval = True):
+        if self.unmodified:
+            return
+        
+        elapsed_time = (now := time.monotonic()) - self.last_time
+        if check_interval and elapsed_time < self.interval:
+            return
+        
+        self.unmodified = True
+        self.last_time = now
+        
+        count_per_sec = self.last_count / elapsed_time
+        self.last_count = 0
+
+        if self.last_size:
+            size_per_sec = f", {FileSizeUtils.format(self.last_size / elapsed_time)}/s"
+            self.last_size = 0
+        else:
+            size_per_sec = ""
+        
+        log.info(f"{self.name}已处理: {self.acc_count}{self.total_count} 项 ({count_per_sec:.1f} 项/秒{size_per_sec})")
+    
+    def stat(self, size: int):
+        self.unmodified = False
+        self.acc_count += 1
+
+        self.last_count += 1
+        self.last_size += size
+
+        self.output()
+    
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, exc_type, exc, tb):
+        self.output(False)
+    
+    def stat_item(self):
+        self.stat(0)
+
+    def stat_file(self, file: File):
+        self.stat(file.size)
+
+class FileCleaner:
+    @classmethod
+    def walk_dir(cls, real_path: str, visited: set[str]):
+        visited.add(real_path)
+
+        with os.scandir(real_path) as entries:
+            for entry in entries:
+                path = os.path.realpath(entry.path)
+                if entry.is_file():
+                    yield File.from_stat(path, entry.stat())
+                elif entry.is_dir():
+                    if path not in visited:
+                        yield from cls.walk_dir(path, visited)
+
+    @classmethod
+    def scan_files(cls, args: Params) -> list[File]:
+        log.info("正在扫描文件 ...")
+
+        files: list[File] = []
+        size = 0
+        with Progress() as p:
+            for file in cls.walk_dir(os.path.realpath(args.dir), set()):
+                if (args.after is None or file.mtime >= args.after) and (args.before is None or file.mtime <= args.before):
+                    files.append(file)
+                    size += file.size
+                    p.stat_item()
+        
+        log.info(f"已发现 {len(files)} 个待清理文件，总计 {FileSizeUtils.format(size)}:")
+
+        files.sort(key=lambda f: f.mtime, reverse=True)
+
+        for i in range(min(10, len(files))):
+            file = files[i]
+            log.info(f"- {file.path} ({FileSizeUtils.format(file.size)}, {datetime.fromtimestamp(file.mtime).strftime("%Y-%m-%d %H:%M:%S")})")
+
+        if files and (args.yes or input(">>> 是否清理文件？[Y/N] ").lower() == "y"):
+            return files
+        
+        return []
+    
+    @staticmethod
+    def remove_files(files: list[File]):
+        if not files:
+            return
+        
+        log.info("正在删除文件 ...")
+
+        size = 0
+        with Progress(total_count=len(files)) as p:
+            for file in files:
+                try:
+                    os.remove(file.path)
+                    # log.debug(f"- {file.path}")
+                    size += file.size
+                    p.stat_file(file)
+                except FileNotFoundError:
+                    pass
+                except Exception as e:
+                    log.warning(f"- 无法删除 {file.path}: {e}")
+        
+        log.info(f"已清理 {len(files)} 个文件，总计 {FileSizeUtils.format(size)}")
+
+    @staticmethod
+    def parse_args():
+        cwd = os.getcwd()
+
+        p = argparse.ArgumentParser(description="文件按日期批量清理")
+        p.add_argument("-d", "--dir", nargs="?", default=cwd, const=cwd, help="扫描目录")
+        p.add_argument("-a", "--after", nargs="?", type=DateTimeUtils.to_mtime, help="起始文件修改时间")
+        p.add_argument("-b", "--before", nargs="?", type=DateTimeUtils.to_mtime, help="结束文件修改时间")
+        p.add_argument("-y", "--yes", action="store_true", help="静默删除")
+        p.add_argument("--no-color", action="store_false", dest="color", help="禁用控制台彩色输出")
+
+        return p.parse_args(namespace=Params())
+    
+    @classmethod
+    def run(cls):
+        args = cls.parse_args()
+        LogFormatter.color_output = args.color
+        log.debug(f"入参: {args}")
+
+        if args.after is None and args.before is None:
+            log.error("起始日期和结束日期不能同时为空")
+            sys.exit(1)
+
+        files = cls.scan_files(args)
+        cls.remove_files(files)
+
 if __name__ == "__main__":
-    FileCleaner().run()
+    FileCleaner.run()
